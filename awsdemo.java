012216e264f3e49afe26fcff9dab302597280e1a
surajhandare <suraj.b@boppotechnologies.com>
2020-09-24 10:38:27 +0000

    Inputoutput Techniques

---
+++ awsdemo.java
@@ -0,0 +1,15 @@
+
+public class awsdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Boolean world");
+		boolean bool1, bool2, result;
+		bool1 = true;
+		bool2 = false;
+		result = bool1 || bool2 ;
+	System.out.println("This is value > " + result);
+	}
+
+}
